cbe260974f049b23880c0bb896a056c3e843b9c7
J. Daniel Escánez <alu0101238944@ull.edu.es>
2021-11-03 18:00:15 +0000

    Cambios en ImageData

--- src/main/python/rgb_effects/model/image_data.py
+++ src/main/python/rgb_effects/model/image_data.py
@@ -1,41 +1,98 @@
 from statistics import mean, stdev
 from PIL import Image
 import os
+import math
 
 class ImageData:
   def __init__(self, image):
     self.image = image
-    self.data = self.image.getdata()
 
-    self.r = []
-    self.g = []
-    self.b = []
-    for sets in self.data:
-      self.r.append(sets[0])
-      self.g.append(sets[1])
-      self.b.append(sets[2])
+    self.r = list(self.image.getdata(band=0))
+    self.g = list(self.image.getdata(band=1))
+    self.b = list(self.image.getdata(band=2))
 
     self.isBw = self.r == self.g == self.b
     self.height = self.image.height
     self.width = self.image.width
+    self.size = self.height * self.width
 
-    self.rRange = self.computeRange(self.r)
-    self.gRange = self.computeRange(self.g)
-    self.bRange = self.computeRange(self.b)
+    self.setHistogramAndRange()
+    self.setAccHistogram()
+    self.setHistogramMean()
+    self.setHistogramStDev()
 
-    self.rBrightness = mean(self.r)
-    self.gBrightness = mean(self.g)
-    self.bBrightness = mean(self.b)
-    self.totalBrightness = mean([self.rBrightness,
-                                 self.gBrightness,
-                                 self.bBrightness])
+  def setHistogramAndRange(self):
+    self.rHistogram = [0] * 256
+    self.gHistogram = [0] * 256
+    self.bHistogram = [0] * 256
 
-    self.rContrast = stdev(self.r)
-    self.gContrast = stdev(self.g)
-    self.bContrast = stdev(self.b)
-    self.totalContrast = mean([self.rContrast,
-                               self.gContrast,
-                               self.bContrast])
+    self.rRange = [256, -1]
+    self.gRange = [256, -1]
+    self.bRange = [256, -1]
 
-  def computeRange(self, array):
-    return (min(array), max(array))
+    for i in range(self.size):
+      rTone = self.r[i]
+      gTone = self.g[i]
+      bTone = self.b[i]
+
+      self.setMinMax(rTone, self.rRange)
+      self.setMinMax(gTone, self.gRange)
+      self.setMinMax(bTone, self.bRange)
+
+      self.rHistogram[rTone] += 1 / self.size
+      self.gHistogram[gTone] += 1 / self.size
+      self.bHistogram[bTone] += 1 / self.size
+
+  def setMinMax(self, x, minMax):
+    if x < minMax[0]:
+      minMax[0] = x
+    if x > minMax[1]:
+      minMax[1] = x
+
+  def setAccHistogram(self):
+    self.rAccHistogram = [0] * 256
+    self.gAccHistogram = [0] * 256
+    self.bAccHistogram = [0] * 256
+
+    for i in range(256):
+      self.rAccHistogram[i] = self.rAccHistogram[i - 1] + self.rHistogram[i]
+      self.gAccHistogram[i] = self.gAccHistogram[i - 1] + self.gHistogram[i]
+      self.bAccHistogram[i] = self.bAccHistogram[i - 1] + self.bHistogram[i]
+
+  def setHistogramMean(self):
+    self.rBrightness = self.gBrightness = self.bBrightness = 0
+
+    for i in range(256):
+      self.rBrightness += i * self.rHistogram[i]
+      self.gBrightness += i * self.gHistogram[i]
+      self.bBrightness += i * self.bHistogram[i]
+
+    self.rBrightness /= self.size
+    self.gBrightness /= self.size
+    self.bBrightness /= self.size
+
+  def setHistogramStDev(self):
+    self.rContrast = self.gContrast = self.bContrast = 0
+
+    for i in range(256):
+      self.rContrast += self.rHistogram[i] * (i - self.rBrightness) ** 2
+      self.gContrast += self.gHistogram[i] * (i - self.gBrightness) ** 2
+      self.bContrast += self.bHistogram[i] * (i - self.bBrightness) ** 2
+
+    self.rContrast = math.sqrt(self.rContrast) / self.size
+    self.gContrast = math.sqrt(self.gContrast) / self.size
+    self.bContrast = math.sqrt(self.bContrast) / self.size
+
+  def setEntropy(self):
+    for i in range(256):
+      rP = self.rHistogram[i] / self.size
+      gP = self.gHistogram[i] / self.size
+      bP = self.bHistogram[i] / self.size
+
+      self.rEntropy += rP * math.log(rP, 2)
+      self.gEntropy += gP * math.log(gP, 2)
+      self.bEntropy += bP * math.log(bP, 2)
+    
+    self.rEntropy *= -1
+    self.gEntropy *= -1
+    self.bEntropy *= -1
