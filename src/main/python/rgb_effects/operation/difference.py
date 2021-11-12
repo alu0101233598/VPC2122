80710f59602255e4f37464d0ed65aa6e464e79a3
Javier Correa <alu0101233598@ull.edu.es>
2021-11-12 20:43:57 +0000

    Implementacion de tabla LUT para la diferencia

--- src/main/python/rgb_effects/operation/difference.py
+++ src/main/python/rgb_effects/operation/difference.py
@@ -2,10 +2,26 @@ from copy import deepcopy
 
 from rgb_effects.model import image_data as id
 
+LUT = []
+
 def calculate_absolute_difference(_, param):
   image_a, image_b, __ = param
   diff_image = deepcopy(image_a)
+
+  if not len(LUT):
+    initialize_lut()
+
   for pixel in range(len(diff_image)):
     for band in range(3):
-      diff_image[band][pixel] = abs(image_a[band][pixel] - image_b[band][pixel])
+      pixel_a = image_a[band][pixel]
+      pixel_b = image_b[band][pixel]
+      value = LUT[pixel_a][pixel_b] if pixel_a < pixel_b else LUT[pixel_b][pixel_a]
+      diff_image[band][pixel] = value
   return id.dataToImage(diff_image)
+
+def initialize_lut():
+  for i in range(256):
+    LUT.append({})
+    for j in range(i, 256):
+      LUT[i][j] = abs(j - i)
+      
