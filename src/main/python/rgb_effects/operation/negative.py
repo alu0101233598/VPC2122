a198f6638b0746ec2be17d6bc0fa314e5c52e7bd
Javier Correa <alu0101233598@ull.edu.es>
2021-11-13 15:33:26 +0000

    Cambio en la LUT de la diferencia

--- src/main/python/rgb_effects/operation/negative.py
+++ src/main/python/rgb_effects/operation/negative.py
@@ -2,10 +2,10 @@ from copy import deepcopy
 
 from rgb_effects.model import image_data as id
 
+LUT = [i for i in reversed(list(range(256)))]
+
 def apply(image_a):
   out_image = deepcopy(image_a)
-
-  LUT = [i for i in reversed(list(range(256)))]
 
   for band in range(3):
     for pixel in range(len(out_image[band])):
