397d01a121c97ff8dc4cceeddb2b905b932ce994
Javier Correa <alu0101233598@ull.edu.es>
2021-11-13 13:23:47 +0000

    Implementacion de la operacion de mapa de cambio

--- src/main/python/rgb_effects/gui/changes_display.py
+++ src/main/python/rgb_effects/gui/changes_display.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QDialog, QDialogButtonBox, QVBoxLayout, QLabel, QMdiArea, QComboBox, QMessageBox
+from PyQt5.QtWidgets import QDialog, QDialogButtonBox, QVBoxLayout, QHBoxLayout, QLabel, QMdiArea, QComboBox, QMessageBox, QSpinBox
 from PyQt5.QtCore import Qt, pyqtSignal
 
 from rgb_effects.common.display_signals import DisplaySignals
@@ -17,6 +17,13 @@ class ChangeDisplay(QDialog):
     layout = QVBoxLayout()
     layout.setSpacing(10)
 
+    layout_v = QHBoxLayout()
+    layout_v.addWidget(QLabel("Threshold"))
+    self.t_spinbox = QSpinBox()
+    self.t_spinbox.setMinimum(0)
+    self.t_spinbox.setMaximum(255)
+    layout_v.addWidget(self.t_spinbox)
+    layout.addLayout(layout_v)
     layout.addWidget(QLabel("Picture A"))
     self.a_combobox = QComboBox()
     self.a_combobox.addItems(self.titles)
@@ -38,5 +45,5 @@ class ChangeDisplay(QDialog):
     if a.image_data.width != b.image_data.width or a.image_data.height != b.image_data.height:
       QMessageBox.critical(self, "Operation failed", "Cannot substract pictures with different sizes.")
       return
-    self.signals.done.emit((a.image_data, b.image_data, {"title": title}))
+    self.signals.done.emit((a.image_data, b.image_data, self.t_spinbox.value(), {"title": title}))
     self.accept()
--- src/main/python/rgb_effects/operation/difference.py
+++ src/main/python/rgb_effects/operation/difference.py
@@ -26,16 +26,22 @@ def calculate_absolute_difference(_, param):
   return id.dataToImage(diff_image)
 
 def calculate_changes(_, param):
-  image_a, image_b, __ = param
-  diff_image = deepcopy(image_a)
+  image_a, image_b, threshold, __ = param
+  change_image = deepcopy(image_a)
 
   if not len(LUT):
     initialize_lut()
 
-  for pixel in range(len(diff_image)):
-    for band in range(3):
-      pixel_a = image_a[band][pixel]
-      pixel_b = image_b[band][pixel]
+  for pixel in range(len(change_image)):
+    current_change = [0, 0, 0]
+    for n, band in enumerate(change_image):
+      pixel_a = image_a[n][pixel]
+      pixel_b = image_b[n][pixel]
       value = LUT[pixel_a][pixel_b] if pixel_a < pixel_b else LUT[pixel_b][pixel_a]
-      diff_image[band][pixel] = value
-  return id.dataToImage(diff_image)
+      if value > threshold:
+        current_change[n] = 255
+    if current_change != [0, 0, 0]:
+      for n in range(3):
+        change_image[n][pixel] = current_change[n]
+  change_image.isGray = False
+  return id.dataToImage(change_image)
