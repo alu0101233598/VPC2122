72867dbdc32c013b6aad442e42ec38fafd08ddd0
J. Daniel Escánez <alu0101238944@ull.edu.es>
2021-12-11 18:13:05 +0000

    Compactando las operaciones de rotacion

--- src/main/python/rgb_effects/operation/rotate.py
+++ src/main/python/rgb_effects/operation/rotate.py
@@ -11,7 +11,7 @@ def rotate_90(image_data):
   for n, _ in enumerate(transposed_data):
     for i in range(transposed_data.height):
       for j in range(transposed_data.width):
-        transposed_data[n][i * transposed_data.width + j] = image_data[n][j * image_data.width + image_data.width - i - 1]
+        transposed_data[n][i * transposed_data.width + j] = image_data[n][(j + 1) * image_data.width - i - 1]
 
   return id.dataToImage(transposed_data)
 
@@ -21,7 +21,7 @@ def rotate_180(image_data):
   for n, _ in enumerate(transposed_data):
     for i in range(transposed_data.height):
       for j in range(transposed_data.width):
-        transposed_data[n][i * transposed_data.width + j] = image_data[n][(image_data.height - i - 1) * image_data.width + image_data.width - j - 1]
+        transposed_data[n][i * transposed_data.width + j] = image_data[n][(image_data.height - i) * image_data.width - j - 1]
 
   return id.dataToImage(transposed_data)
 
