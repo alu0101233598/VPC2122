9a6f6942a434b9617e17b4ce5b12523d8366e74d
J. Daniel Escánez <alu0101238944@ull.edu.es>
2021-12-16 18:42:15 +0000

    Errores generales de la escala arreglados

--- src/main/python/rgb_effects/operation/geometric_transformation.py
+++ src/main/python/rgb_effects/operation/geometric_transformation.py
@@ -22,18 +22,11 @@ def apply(input_data, size_out_image, coordinates_map, interpolation_method):
   output_data.b = [0] * output_data.size
 
   for i in range(0, output_data.height):
-    for j in range(output_data.width - 3, output_data.width):
-      border(input_data, output_data, interpolation_method, coordinates_map, i, j)
-
-  for i in range(output_data.height - 3, output_data.height):
     for j in range(0, output_data.width):
-      border(input_data, output_data, interpolation_method, coordinates_map, i, j)
-
-
-  for i in range(0, output_data.height - 3):
-    for j in range(0, output_data.width - 3):
       pixel_it = i * output_data.width + j
       x, y = coordinates_map(j, i)
+      x = x if x < input_data.width - 2 else input_data.width - 2
+      y = y if y < input_data.height - 2 else input_data.height - 2
       p = x - floor(x)
       q = y - floor(y)
       for n, _ in enumerate(output_data):
@@ -45,16 +38,3 @@ def apply(input_data, size_out_image, coordinates_map, interpolation_method):
         output_data[n][pixel_it] = interpolation_methods[interpolation_method](p, q, a, b, c, d)
 
   return id.dataToImage(output_data)
-
-def border(input_data, output_data, interpolation_method, coordinates_map, i, j):
-  pixel_it = i * output_data.width + j
-  x, y = coordinates_map(j, i)
-  p = x - floor(x) if x < output_data.height - 1 else 0
-  q = y - floor(y) if y < output_data.width - 1 else 0
-  for n, _ in enumerate(output_data):
-    c = input_data[n][floor(y) * input_data.width + floor(x)]
-    a = input_data[n][(floor(y) + 1) * input_data.width + floor(x)] if (floor(y) + 1) * input_data.width + floor(x) < len(input_data[n]) else c
-    b = input_data[n][(floor(y) + 1) * input_data.width + (floor(x) + 1)] if (floor(y) + 1) * input_data.width + (floor(x) + 1) < len(input_data[n]) else c
-    d = input_data[n][floor(y) * input_data.width + (floor(x) + 1)] if floor(y) * input_data.width + (floor(x) + 1) < len(input_data[n]) else c
-    
-    output_data[n][pixel_it] = interpolation_methods[interpolation_method](p, q, a, b, c, d)
