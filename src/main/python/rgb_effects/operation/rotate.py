66cb1a92d40e6dd92ac3b6e4fca626c32ceffefc
J. Daniel Escánez <alu0101238944@ull.edu.es>
2021-12-16 19:14:57 +0000

    Display y parte de la funcionalidad implementados

--- src/main/python/rgb_effects/operation/rotate.py
+++ src/main/python/rgb_effects/operation/rotate.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from copy import deepcopy
+from math import sin, cos
 
 from rgb_effects.model import image_data as id
 
@@ -36,3 +37,11 @@ def rotate_270(image_data):
         transposed_data[n][i * transposed_data.width + j] = image_data[n][(image_data.height - j - 1) * image_data.width + i]
 
   return id.dataToImage(transposed_data)
+
+def apply(input_data, param):
+  angle, interpolation_method, __ = param
+  coordinates_map_dt = lambda i, j: (i * cos(angle) - j * sin(angle), i * sin(angle) + j * cos(angle))
+  coordinates_map_it = lambda i, j: (i * cos(- angle) - j * sin(- angle), i * sin(- angle) + j * cos(- angle))
+
+  size_out_image = # TODO
+  return geometric_transformation.apply(input_data, size_out_image, coordinates_map_it, interpolation_method)
